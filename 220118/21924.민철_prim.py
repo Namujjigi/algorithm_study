b337ae565af92221065769934b25d76fba2f33f5
unknown <kmc03047@naver.com>
2022-01-19 02:26:24 +0900

    0118 알고리즘 풀이 추가

--- 220118/21924.민철_prim.py
+++ 220118/21924.민철_prim.py
@@ -1,6 +1,10 @@
+# 프림 로직을 쓰는데 최소점을 돌리는걸 heapq로 해결하는 법
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 G = [[] for _ in range(N + 1)]
-print(G)
 total_cost = 0
 
 for _ in range(M):
@@ -14,21 +18,20 @@ key = [123456789 for _ in range(N + 1)]
 # 난 1번부터 돌거임
 key[1] = 0
 min_cost = 0
-for _ in range(1, N + 1):
-    u, min_key = 0, 123456789
-    for i in range(1, N + 1):
-        if not visit[i] and min_key > key[i]:
-            u, min_key = i, key[i]
+# 가중치 시작할 정점 위치
+Q = [[0, 1]]
+while Q:
+    d, u = heappop(Q)
+    if visit[u]:
+        continue
     visit[u] = 1
-    min_cost += min_key
-
+    min_cost += d
     for v, w in G[u]:
-        if not visit[v] and key[v] > w:
-            key[v] = w
+        if not visit[v]:
+            heappush(Q, [w, v])
 
 if sum(visit) == N:
     print(total_cost - min_cost)
 else:
     print(-1)
 
-# 프림 로직을 쓰는데 최소점을 돌리는걸 heapq로 해결하는 법
