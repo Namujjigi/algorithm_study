c8af0b590a78b41c31c21073e8b3c71b745f0dfd
unknown <kmc03047@naver.com>
2022-01-29 23:46:15 +0900

    0129 study

--- 220127/boj_6987_mc.py
+++ 220127/boj_6987_mc.py
@@ -1,29 +1,42 @@
-# 현재 나라, 상대할 적, 결과, 경기 수 판단
-def backtrack(country, opponent, cnt):
-    if country == 5:
-        print('전적학인')
-        for ma in match:
-            print(ma, end='\n')
-        print()
+def make_match(start, cnt):
+    if cnt == 2:
+        match_lst.append(pick[::])
         return
 
-    elif opponent == 6:
-        backtrack(country + 1, country + 2, 0)
+    for i in range(start + 1, 6):
+        pick.append(i)
+        make_match(i, cnt + 1)
+        pick.pop()
 
-    # 0, 1, 2 / 승 무 패
-    elif cnt < 5 - country:
-        if not visit[country][opponent]:
-            visit[country][opponent] = 1
-            for i in range(3):
-                match[country][i] += 1
-                match[opponent][2 - i] += 1
-                backtrack(country, opponent + 1, cnt + 1)
-                match[country][i] -= 1
-                match[opponent][2 - i] -= 1
-            visit[country][opponent] = 0
+def match_result(idx):
+    global re
+    if idx == 15:
+        re += 1
+        for k in range(0, 18, 3):
+            print(match_result_lst[k:k+3], end='\n')
+        print()
+        print(re)
+        return
 
-match = [[0 for _ in range(3)] for _ in range(6)]
-visit = [[0 for _ in range(6)] for _ in range(6)]
+    # 0, 1, 2/ 승 무 패
+    team1 = match_lst[idx][0]
+    team2 = match_lst[idx][1]
+    for i in range(3):
+        team1_result = i
+        team2_result = 2 - i
+        match_result_lst[team1 * 3 + team1_result] += 1
+        match_result_lst[team2 * 3 + team2_result] += 1
+        match_result(idx + 1)
+        match_result_lst[team1 * 3 + team1_result] -= 1
+        match_result_lst[team2 * 3 + team2_result] -= 1
 
-backtrack(0, 1, 0)
 
+pick = []
+match_lst = []
+match_result_lst = [0 for _ in range(18)]
+re = 0
+make_match(-1, 0)
+for ma in match_lst:
+    print(ma, end='\n')
+print()
+match_result(0)
